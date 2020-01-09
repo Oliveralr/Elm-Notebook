679adb7211601c26c422d1009717076a1bd21f25
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2020-01-09 01:43:27 -0600

    Shiped!

---
+++ first.elm
@@ -0,0 +1,30 @@
+-- expresions
+var = 
+    "HELLO!"
+
+value =
+    32
+
+-- concat
+vartwo = 
+    "HELLO" ++ "WORLD"
+
+-- functions
+prints name = 
+    "My Name is" ++ name ++ "!"
+
+prints "Jhon Doe"
+
+prints name job =
+    "My name is" ++ name ++ "and I'm a" ++ job
+
+prints ("Jhon" ++ " Doe") "programmer"
+
+-- if/else statements
+evaluate state =
+    if state == true then
+        "Available"
+    else
+        "Unavailable"
+
+evaluate true
